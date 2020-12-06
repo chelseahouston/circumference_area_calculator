a2f5c9cebb165c6187b1115ef148658ce7667d53
Chelsea Houston <chelseahouston29@gmail.com>
2020-12-06 20:03:10 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Circumference/Area Calculator
+A program that calculates the area and circumference of a circle when given a diameter.
+
+## Installation
+No installation required to run this project.
+```
+C++ main.cpp
+```
+
+Created as part of TechTalent Academy Women in Software course.
---
+++ main.cpp
@@ -0,0 +1,25 @@
+//calcult
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  double diameter, d, radius, r, circumference, c, a; // -- declare all var as double | account for decimals
+
+  // task 01 --- decalre constant pi | calculate circumference with diameter of 6
+  const double pi = 3.14159265359;
+  diameter = 6;
+  radius = diameter / 2; // calculate radius
+  circumference = 2 * pi * radius; // calculate circumference
+  cout << "The circumference of a circle with a diameter of 6 is " << circumference; // output circumference
+
+  // task 02 --- user input diameter | calculate circumference and area
+  cout << "\n\nPlease enter the diameter of a circle to work out the circumference: ";
+  cin >> d; //user input diameter (d)
+  r = d/ 2; // calculate radius (r)
+  c = 2 * pi * r; // calculate circumference (c)
+  a = pi * (r * r); // calculate area (a)
+  cout << "The circumference of a circle with a diameter of " << d << " is "<< c; // output circumference
+  cout << "\nThe area of a circle with a diameter of " << d << " is " << a; // output area
+
+}
